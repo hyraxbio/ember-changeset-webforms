6a57714d64361cba7b170983c169428c75a44f1a
Andrew Paterson <andrew@hyraxbio.co.za>
2019-07-26 12:16:19 +0200

    Added ability to parse sub field Ids.

--- addon/utils/generate-form-values.js
+++ addon/utils/generate-form-values.js
@@ -1,24 +1,34 @@
 export default function generateFormValues(formFields) {
-  var values = {};
-    formFields.forEach(function(field) {
-      if (field.fieldType === 'staticContent' || field.hidden) {return;}
-      var levels = field.fieldId.split(".");
-      var acc = values;
+  var final = {};
+  formFields.forEach(function(field) {
+    function processField(levels, final, value) {
+      var acc = final;
       levels.forEach(function(level, index) {
         if (index === levels.length-1) {
           if (field.includeLabelOnSubmit) {
             acc[level] = {
               label: field.fieldLabel,
-              value: field.value
+              value: value
             };
           } else {
-            acc[level] = field.value;
+            acc[level] = value;
           }
         } else {
           acc[level] = acc[level] || {};
           acc = acc[level];
         }
       });
-    });
-    return values;
+    }
+    if (field.fieldType === 'staticContent' || field.hidden) {return;}
+    if (field.fieldSubIds) {
+      field.fieldSubIds.forEach(subId => {
+        var levels = `${field.fieldId}${subId.id}`.split('.');
+        processField(levels, final, field.value[subId.valueKey]);
+      });
+    } else {
+      var levels = field.fieldId.split(".");
+      processField(levels, final, field.value);
+    }
+  });
+  return final;
 }
