4625da1509879095446c0b8d0db1a264f6e9d5b6
Andrew Paterson <andrew@hyraxbio.co.za>
2019-07-31 15:21:53 +0200

    Updates to power date picker field.

--- addon/services/ember-pojo-forms.js
+++ addon/services/ember-pojo-forms.js
@@ -1,7 +1,17 @@
 import Service from '@ember/service';
 
 export default Service.extend({
-  submitButtonIcon: 'ember-pojo-form/submit-button-icon',
-  submitButtonIconClassNames: 'button-right spinner',
-  submitButtonIconRequestInFlightClassNames: 'on'
+  init() {
+    this._super(...arguments);
+    this.settings = {
+      submitButtonIcon: 'ember-pojo-form/submit-button-icon',
+      submitButtonIconClassNames: 'button-right spinner',
+      submitButtonIconRequestInFlightClassNames: 'on',
+      powerDatePicker: {
+        dateSelectComponent: null
+      }
+    };
+  }
+  
+  
 });
