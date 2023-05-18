576e1a90f19d9af2dcb4f98cd41557912e83491f
Andrew Paterson <andrew.paterson9@gmail.com>
2023-05-18 16:10:30 +0200

    Address no-implicit-this deprecations

--- addon/components/ember-changeset-webforms/field-elements/validating-field-wrapper.js
+++ addon/components/ember-changeset-webforms/field-elements/validating-field-wrapper.js
@@ -4,40 +4,82 @@ import { computed } from '@ember/object';
 import dynamicClassNames from 'ember-changeset-webforms/utils/dynamic-class-names';
 
 export default Component.extend({
-  tagName: "",
+  tagName: '',
   layout,
 
-  'dataTestId': computed('dataTestFieldId', function() {
-    if (!this.dataTestFieldId) { return; }
+  dataTestId: computed('dataTestFieldId', function () {
+    if (!this.dataTestFieldId) {
+      return;
+    }
     return `${this.dataTestFieldId}-field`;
   }),
 
-  'dataTestClass': computed('typeClass', function() {
-    if (!this.typeClass) { return; }
+  dataTestClass: computed('typeClass', function () {
+    if (!this.typeClass) {
+      return;
+    }
     return `cwf-${this.typeClass}`;
   }),
 
-  fieldWrapperClassNames: computed('changesetWebform', 'formField.validationStatus', function() {
-    return dynamicClassNames('fieldWrapper', this.changesetWebform, this.formField);
+  fieldWrapperClassNames: computed(
+    'changesetWebform',
+    'formField.validationStatus',
+    function () {
+      return dynamicClassNames(
+        'fieldWrapper',
+        this.changesetWebform,
+        this.formField
+      );
+    }
+  ),
+
+  requiredClasses: computed('formField.required', function () {
+    return this.formField.required
+      ? dynamicClassNames(
+          'requiredField',
+          this.changesetWebform,
+          this.formField
+        )
+      : '';
   }),
 
-  requiredClasses: computed('formField.required', function() {
-    return this.formField.required ? dynamicClassNames('requiredField', this.changesetWebform, this.formField) : '';
+  disabledClasses: computed('formField.disabled', function () {
+    return this.formField.disabled
+      ? dynamicClassNames(
+          'disabledField',
+          this.changesetWebform,
+          this.formField
+        )
+      : '';
   }),
 
-  disabledClasses: computed('formField.disabled', function() {
-    return this.formField.disabled ? dynamicClassNames('disabledField', this.changesetWebform, this.formField) : '';
+  validatesClasses: computed('formField.validates', function () {
+    return this.formField.validates
+      ? dynamicClassNames(
+          'fieldValidates',
+          this.changesetWebform,
+          this.formField
+        )
+      : '';
   }),
 
-  validatesClasses: computed('formField.validates', function() {
-    return this.formField.validates ? dynamicClassNames('fieldValidates', this.changesetWebform, this.formField) : '';
+  wasValidatedClasses: computed('formField.wasValidated', function () {
+    return this.formField.wasValidated
+      ? dynamicClassNames(
+          'validatedField',
+          this.changesetWebform,
+          this.formField
+        )
+      : '';
   }),
 
-  wasValidatedClasses: computed('formField.wasValidated', function() {
-    return this.formField.wasValidated ? dynamicClassNames('validatedField', this.changesetWebform, this.formField) : '';
+  focussedClasses: computed('formField.focussed', function () {
+    return this.formField.focussed
+      ? dynamicClassNames(
+          'focussedField',
+          this.changesetWebform,
+          this.formField
+        )
+      : '';
   }),
-
-  focussedClasses: computed('formField.focussed', function() {
-    return this.formField.focussed ? dynamicClassNames('focussedField', this.changesetWebform, this.formField) : '';
-  })
 });
