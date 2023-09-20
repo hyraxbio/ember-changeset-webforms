6b4c949d7be01462e239ede7d65560fa835db5d5
Andrew Paterson <andrew.paterson9@gmail.com>
2023-09-20 14:15:17 +0200

    Bugfix

--- addon/components/ember-changeset-webforms/svg/icons/svg-icon-base.js
+++ addon/components/ember-changeset-webforms/svg/icons/svg-icon-base.js
@@ -1,12 +1,12 @@
 import { tracked } from '@glimmer/tracking';
 import { tagName } from '@ember-decorators/component';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
 @tagName('')
 export default class SvgIconBase extends Component {
   @tracked vectorEffect;
 
   get computedVectorEffect() {
-    return this.vectorEffect || 'non-scaling-stroke';
+    return this.args.vectorEffect || 'non-scaling-stroke';
   }
 }
--- tests/dummy/app/controllers/docs/basic-usage.js
+++ tests/dummy/app/controllers/docs/basic-usage.js
@@ -1,6 +1,7 @@
 import ApplicationController from '../application';
-import { alias } from '@ember/object/computed';
 
 export default ApplicationController.extend({
-  formSchema: alias('loginFormSchema'),
+  get formSchema() {
+    return this.loginFormSchema;
+  },
 });
