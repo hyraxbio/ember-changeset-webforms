ed024ad2ffb8839edc305aede1b4414108e9b166
Andrew Paterson <andrew.paterson9@gmail.com>
2024-01-30 15:03:19 +0200

    Add tracked property to option class

--- addon/utils/option-class.js
+++ addon/utils/option-class.js
@@ -2,6 +2,7 @@ import { tracked } from '@glimmer/tracking';
 
 export default class Option {
   @tracked value;
+  @tracked onlyCheckedOption;
   constructor(args) {
     for (const key in args) {
       this[key] = args[key];
