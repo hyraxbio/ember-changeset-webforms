087517d3282a08c002d71c743d0db4cf0b38b42f
Andrew Paterson <andrew.paterson9@gmail.com>
2024-08-01 15:49:58 +0200

    More

--- addon/components/ember-changeset-webforms/field-elements/field-label-wrapper.js
+++ addon/components/ember-changeset-webforms/field-elements/field-label-wrapper.js
@@ -3,7 +3,7 @@ import Component from '@glimmer/component';
 export default class FieldLabel extends Component {
   get labelType() {
     const formField = this.args.formField;
-    const nonFormElementFieldTypes = ['staticContent'];
+    const nonFormElementFieldTypes = ['staticContent', 'powerSelect'];
     if (nonFormElementFieldTypes.includes(formField.fieldType)) {
       return 'div';
     }
